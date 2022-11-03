2bc6e4c1360a6a4bb2822b3570a63a2ac8df19d9
THONG\Admin <thongnq@fsoft.com.vn>
2022-11-03 20:21:58 +0700

    commit config

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'a7d4b4231bb094ad08c65bba29b1121f-1871223610.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a7d4b4231bb094ad08c65bba29b1121f-1871223610.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
